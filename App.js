a950d7c00edde016d1fc55b2229b95ae26968f47
Railand50 <72070000+Railand50@users.noreply.github.com>
2020-10-24 23:41:49 +0800

    Deleted previous app

--- App.js
+++ App.js
@@ -1,61 +1,28 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View, TouchableOpacity } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import HomeScreen from "./Screens/HomeScreen"
+import DetailsScreen from "./Screens/DetailsScreen"
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}> 
-        <Text style={styles.title}>Home</Text>  
-      </View>
-      <View style={styles.body}>
-        <TouchableOpacity onPress={() => console.log()} style={styles.button}>         
-          <Text style={styles.buttonTitle}>Click Me</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log()} style={styles.button}>         
-          <Text style={styles.buttonTitle}>Click Me</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create ({
-  container: {
-    flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'space-between',   
-  },
-
-  header: {
-    height: 100,  
-  },
-
-  title: {
-    textAlign: 'left',
-    fontWeight: 'bold',
-    fontSize: 60,
-    marginTop: 40,
-    marginLeft: 20,
-    color: '#18435A',
-  },
-
-  body: {
-    
-  },
-
-  button: {
-    margin: 20,
-    backgroundColor: '#18435A',
-    alignItems: 'center',
-    justifyContent: 'center',  
-    borderRadius: 20
-
-  },
-
-  buttonTitle: {
-    margin: 20,
-    color: 'white'
-  },
-
-  
-});
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: "Home"}}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{ title: "Details" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+  }
---
+++ Screens/DetailsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function DetailsScreen() {
+	return (
+		<SafeAreaView>
+			<Text>This is Details Screen</Text>
+		</SafeAreaView>
+	);
+} 
